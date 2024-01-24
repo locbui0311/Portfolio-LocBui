dbab88dddb918b7b4b632ff9fb597813e2a7ab75
unknown <locbui.it.0311@gmail.com>
2024-01-24 16:26:07 +0700

    add background color

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import {BiHomeAlt, BiUser} from 'react-icons/bi';
+import {BsClipboardData, BsBriefcase, BsChatSquare} from 'react-icons/bs';
+import {Link} from 'react-scroll';
 
 const Nav = () => {
-  return <div>Nav</div>;
+
+  return <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+    <div className='container mx-auto'>
+      {}
+      <div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl'>
+         <Link>
+         <BiHomeAlt/>
+         </Link>
+      </div>
+
+    </div>
+  </nav>;
 };
 
 export default Nav;
