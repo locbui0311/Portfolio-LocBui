3f789b29f83ac262f394bb5603756d3609621bf1
unknown <locbui.it.0311@gmail.com>
2024-01-25 17:12:58 +0700

    add icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
-import React from 'react';
-
+import React from "react";
+import Logo from "../assets/logo.svg";
+import { Button } from "react-scroll";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div>
+          {/*lo go */}
+          <a href="#">
+            <img src={Logo} alt="" />
+          </a>
+          <button className="btn btn-sm"> Work with me </button>
+        </div>
+      </div>
+      Header
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,71 @@
-import React from 'react';
-import {BiHomeAlt, BiUser} from 'react-icons/bi';
-import {BsClipboardData, BsBriefcase, BsChatSquare} from 'react-icons/bs';
-import {Link} from 'react-scroll';
+import React from "react";
+import { BiHomeAlt, BiSolidContact, BiUser } from "react-icons/bi";
+import {
+  BsClipboardData,
+  BsBriefcase,
+  BsChatSquare,
+  BsChatSquareText,
+} from "react-icons/bs";
+import { Link } from "react-scroll";
 
 const Nav = () => {
-
-  return <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
-    <div className='container mx-auto'>
-      {}
-      <div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl'>
-         <Link>
-         <BiHomeAlt/>
-         </Link>
+  return (
+    <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+      <div className="container mx-auto">
+        {}
+        <div
+          className="w-full bg-black/20 h-[96px] backdrop-blur-2xl
+        rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50"
+        >
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="home"
+            className=" cursor-pointer w-[60px] h-[60px] flex items-center jus"
+          >
+            <BiHomeAlt />
+          </Link>
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="about"
+            className=" cursor-pointer w-[60px] h-[60px] flex items-center jus"
+          >
+            <BiUser />
+          </Link>
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="services"
+            className=" cursor-pointer w-[60px] h-[60px] flex items-center jus"
+          >
+            <BsClipboardData />
+          </Link>
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="work"
+            className=" cursor-pointer w-[60px] h-[60px] flex items-center jus"
+          >
+            <BsBriefcase />
+          </Link>
+          <Link
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            to="contact"
+            className=" cursor-pointer w-[60px] h-[60px] flex items-center jus"
+          >
+            <BsChatSquareText />
+          </Link>
+        </div>
       </div>
-
-    </div>
-  </nav>;
+    </nav>
+  );
 };
 
 export default Nav;
