d19efab8a7930e158d0e704cf1e8a68488057657
unknown <locbui.it.0311@gmail.com>
2024-02-27 16:04:16 +0700

    complete page Banner and add logo avatar

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import React from "react";
 //images
-import Image from "../assets/avatar.svg";
+import Image from "../assets/TamHai.gif";
 //icons
 import { FaGithub, FaYoutube, FaDribbble, FaInstagram } from "react-icons/fa";
 // type animation
@@ -12,15 +12,28 @@ import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section className="section" id="home">
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+      id="home"
+    >
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center">
           {/* text */}
-          <div>
-            <h1>
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="  text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+            >
               LOC <span> BUI</span>
-            </h1>
-            <div
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
               className="mb-6 text-[36px] lg:text-[60px] font-secondary
             font-semibold uppercase leading-[1]"
             >
@@ -29,7 +42,7 @@ const Banner = () => {
                 sequence={[
                   "Developer",
                   2000,
-                  "Wordpress",
+                  "Youtuber",
                   2000,
                   "Designer",
                   2000,
@@ -39,16 +52,34 @@ const Banner = () => {
                 wrapper="span"
                 repeat={Infinity}
               />
-            </div>
-            <p className="mb-8 max-w-lg mx-auto lg:mx-0">Lorem ipsum...</p>
-            <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+              Lorem ipsum...
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
               <button className="btn btn-lg"> Contact me </button>
               <a href="#" className="text-gradient btn-link">
                 My Portfolio
               </a>
-            </div>
+            </motion.div>
             {/* socials */}
-            <div
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
               className="flex text-[20px] gap-x-6 max-w-max mx-auto
             lg:mx-0 "
             >
@@ -61,12 +92,18 @@ const Banner = () => {
               <a href="#">
                 <FaInstagram />
               </a>
-            </div>
+            </motion.div>
           </div>
           {/* image */}
-          <div>
+          <motion.div
+            variants={fadeIn("up", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]"
+          >
             <img src={Image} alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../assets/logo.svg";
+import Logo from "../assets/logo.png";
 import { Button } from "react-scroll";
 const Header = () => {
   return (
