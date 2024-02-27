5d284099464bda816fa61a8b9749b8e2d306ebf9
unknown <locbui.it.0311@gmail.com>
2024-02-27 16:13:03 +0700

    add librar code

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+//CountUp
+import CountUp from "react-countup";
+//motion
+import { useInView } from "framer-motion";
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
 const About = () => {
   return (
     <div className="section" id="about">
